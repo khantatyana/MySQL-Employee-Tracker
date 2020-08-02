78210baa476fba8cda0d82baaeb408bae700d12f
Tatyana <hanyatan@gmail.com>
2020-08-02 00:27:42 -0400

    added view dpts, roles, add new dpt, exit functions

---
+++ database/index.js
@@ -0,0 +1,29 @@
+const connection = require("./connect")
+
+class Database {
+    constructor(connection) {
+        this.connection = connection;
+    }
+    allEmplFunc() {
+        return this.connection.query(
+            "SELECT employee.id AS Employee_id, employee.first_name AS First_Name, employee.last_name AS Last_Name, role.title AS Role_Title, department.name AS Department, role.salary AS Salary, CONCAT(manager.first_name, ' ', manager.last_name) AS Manager FROM employees_DB.employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+            );
+    }
+    allDptFunc() {
+        return this.connection.query(
+            "SELECT department.id AS Dpt_id, department.name AS Department, SUM(role.salary) AS Aggregate_Salary FROM employees_DB.department LEFT JOIN role on role.department_id = department.id LEFT JOIN employee on employee.role_id = role.id GROUP BY department.id, department.name;"
+            );
+    }
+    allRolesFunc(){
+        return this.connection.query(
+            "SELECT department.id AS Dpt_id, department.name AS Department, role.id AS Role_id, role.title AS Role_title, role.salary AS Salary FROM role LEFT JOIN department on role.department_id = department.id ORDER by department;"
+            );
+    }
+    newDptFunc(dpt){
+        return this.connection.query("INSERT INTO department SET ?", dpt);
+    }
+    newEmployee(employee) {
+        return this.connection.query("INSERT INTO employee SET ?", employee);
+      }
+}
+module.exports = new Database(connection);
--- main.js
+++ main.js
@@ -1,23 +1,10 @@
 const inquirer = require("inquirer");
-var mysql = require("mysql");
-// const db = require("./database");
+// const connection = require("./database/connect")
+const db = require("./database");
 const disp = require("asciiart-logo");
 require("console.table");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "employees_DB"
-});
-
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-  start();
-});
-
+start();
 function start() {
     const display = disp({name: "Employee Tracker"}).render();
     console.log(display);
@@ -30,116 +17,165 @@ function allPrompts() {
         message: "Whay would you like to do?",
         choices: [
             "View all Employees",
-            "View employees by manager",
             "View all Departments",
             "View all Roles",
-            "Update employee role",
-            "Update employee manager",
-            "Add a department",
-            "Add an employee",
-            "Add a role",
-            "Delete department",
-            "Delete role. WARNING: this operation is going to delete all employees with this role!",
-            "Delete employee",
+            "Update Employee Role",
+            "Update Employee Manager",
+            "Add a Department",
+            "Add an Employee",
+            "Add a Role",
+            "Delete Department",
+            "Delete Role. WARNING: this operation is going to delete all employees with this role!",
+            "Delete Employee",
             "Exit"
         ]
     }).then(function(answers) {
         if (answers.employee_tracker === "View all Employees") {
             viewAllEmployeesFunc();
-        } else if (answers.employee_tracker === "View employees by manager") {
-            viewEmplByManagerFunc();
         } else if (answers.employee_tracker === "View all Departments") {
             viewAllDptsFunc();
         } else if (answers.employee_tracker === "View all Roles") {
             viewAllRolesFunc();
-        } else if (answers.employee_tracker === "Update employee role") {
+        } else if (answers.employee_tracker === "Update Employee Role") {
             updateEmplRoleFunc();
-        } else if (answers.employee_tracker === "Update employee manager") {
+        } else if (answers.employee_tracker === "Update Employee Manager") {
             updateEmplManagerFunc();
-        } else if (answers.employee_tracker === "Add a department") {
+        } else if (answers.employee_tracker === "Add a Department") {
             addDptFunc();
-        } else if (answers.employee_tracker === "Add an employee") {
+        } else if (answers.employee_tracker === "Add an Employee") {
             addEmployeeFunc();
-        } else if (answers.employee_tracker === "Add a role") {
+        } else if (answers.employee_tracker === "Add a Role") {
             addRoleFunc();
-        } else if (answers.employee_tracker === "Delete department") {
+        } else if (answers.employee_tracker === "Delete Department") {
             deleteDptFunc();
-        } else if (answers.employee_tracker === "Delete role. WARNING: this operation is going to delete all employees with this role!") {
+        } else if (answers.employee_tracker === "Delete Role. WARNING: this operation is going to delete all employees with this role!") {
             deleteRoleFunc();
-        } else if (answers.employee_tracker === "Delete employee") {
+        } else if (answers.employee_tracker === "Delete Employee") {
             deleteEmployeeFunc();
         } else if (answers.employee_tracker === "Exit") {
-            console.log("Good-bye!")
-            connection.end();
+            endFunc();
         } else {
             connection.end();
         }
     })
 }
-
-function viewAllEmployeesFunc() {
-    connection.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employees_DB.employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;", 
-    function(err, res) {
-        if(err) {
-            throw err;
-        }
-        console.log("\n");
-        console.table(res);
-        allPrompts();
-        
-    });
+async function viewAllEmployeesFunc() {
+    const res = await db.allEmplFunc();
+    console.log("\n");
+    console.table(res);
+    allPrompts();
 }
-function viewEmplByManagerFunc() {
-    // connection.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employees_DB.employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;", 
-    // function(err, res) {
-    //     if(err) {
-    //         throw err;
-    //     }
-    //     console.log("\n");
-    //     console.table(res);
-    //     allPrompts();
-        
-    // });
-    inquirer.prompt([
+async function viewAllDptsFunc() {
+    const res = await db.allDptFunc();
+    console.log("\n");
+    console.table(res);
+    allPrompts();
+}
+async function viewAllRolesFunc() {
+    const res = await db.allRolesFunc();
+    console.log("\n");
+    console.table(res);
+    allPrompts();
+}
+async function addDptFunc(){
+    const dpt = await inquirer.prompt([
         {
-            name: "item",
+            name: "name",
             type: "input",
-            message: "What is the item you would like to post?"
-        },
-        {
-            name: "category",
-            type: "input",
-            message: "What is the category you would like to post?"
-        },
-        {
-            name: "starting_price",
-            type: "input",
-            message: "What is the starting_price?"
-        },
-        {
-            name: "quantity",
-            type: "input",
-            message: "What is the quantity?"
+            message: "Please enter name of the new Department."
         }
-    ]).then(
-       function(answers) {
-           connection.query("INSERT INTO auctions SET ?", {
-               item: answers.item,
-               category: answers.category,
-               starting_price: answers.starting_price,
-               high_price: answers.starting_price,
-               quantity: answers.quantity
+    ]);
+    await db.newDptFunc(dpt);
+    const dpts = await db.allDptFunc();
+    if (dpts.includes(dpt.name)) {
+        console.log(`${dpt.name} is already existing. Try again.`)
+        addDptFunc();
+    }
+    console.log(`${dpt.name} was added to the database.`)
+    allPrompts();
+}
+async function addEmployeeFunc(){
+    const roles = await db.allRolesFunc();
+    const rolechoices = []; 
+    function defineAllRoles() {
+        for (var i = 0; i < roles.length; i ++) {
+            rolechoices.push(roles[i].Role_title)
+        }
+        return rolechoices;
+    };
+    defineAllRoles();
 
-           }, function(err){
-               if (err) {
-                   throw err;
-               }
-               console.log("created successfully!");
-               start();
-           })
-       } 
-    )
+    const managers = await db.allEmplFunc();
+    const managerchoices = [];
+    function defineAllManagers() {
+        for (var i = 0; i < managers.length; i ++) {
+            let firstName = managers[i].First_Name;
+            let lastName = managers[i].Last_Name;
+            managerchoices.push(`${firstName} ${lastName}`);
+        }
+        return managerchoices;
+    };
+    defineAllManagers();
+    console.log(managers);
+    console.log(managerchoices);
+    const employee = await inquirer.prompt([
+        {
+            name: "first_name",
+            message: "Please enter employee's first name."
+        },
+        {
+            name: "last_name",
+            message: "Please enter employee's last name."
+        },
+        {
+            name: "role_id",
+            type: "list",
+            message: "Please enter employee's role.",
+            choices: rolechoices
+        },
+        {
+            name: "manager_id",
+            type: "list",
+            message: "Please enter employee's manager",
+            choices: managerchoices
+        }
+    ]);
+    await db.newEmployee(employee);
+    console.log(`${employee.first_name} ${employee.last_name} was added.`);
+    allPrompts();
 }
 
 
+// function updateEmplRoleFunc() {
 
+//     inquirer.prompt([
+//         {
+//             name: "managers",
+//             type: "list",
+//             message: "Choose manager?",
+//             choices: managers
+//         }
+//     ]).then(
+//        function(answers) {
+//            connection.query("INSERT INTO auctions SET ?", {
+//                item: answers.item,
+//                category: answers.category,
+//                starting_price: answers.starting_price,
+//                high_price: answers.starting_price,
+//                quantity: answers.quantity
+
+//            }, function(err){
+//                if (err) {
+//                    throw err;
+//                }
+//                console.log("created successfully!");
+//                start();
+//            })
+//        } 
+//     )
+// }
+
+function endFunc() {
+    console.log("Good-bye!");
+    process.exit();
+}
